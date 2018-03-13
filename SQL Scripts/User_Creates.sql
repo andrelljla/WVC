315a4d6ac523b59689349c50ea7e24ff37c3f816
andrelljla <jandrews@andrellsolutions.com>
2018-03-13 14:11:56 -0500

    Editorial Changes
    Editorial cleanup

--- SQL Scripts/User_Creates.sql
+++ SQL Scripts/User_Creates.sql
@@ -1,11 +1,9 @@
 select * from all_users;
 
-drop user wvc;
+drop user wvc cascade;
 create user wvc identified by wvc default tablespace users;
-grant connect, create session, resource to wvc;
+grant connect, create session, resource, create table, create view, create procedure, create trigger to wvc;
 
-drop user jla;
+drop user jla cascade;
 create user jla identified by jla default tablespace users;
 grant connect, create session, resource, dba to jla;
-
-
